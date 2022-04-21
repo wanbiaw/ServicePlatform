3a991f944d19bdab5cb4a08c69bcc29c79979531
wangxiang10 <wangxiang10@xiaomi.com>
2022-04-21 19:27:14 +0800

    first commit

---
+++ src/main/java/com/xiaomi/xiaoai/servicefunc/ServicefuncApplication.java
@@ -0,0 +1,13 @@
+package com.xiaomi.xiaoai.servicefunc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicefuncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicefuncApplication.class, args);
+    }
+
+}
